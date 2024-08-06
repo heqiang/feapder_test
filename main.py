de9aa8bb253f869357bef9d9dc083ca0512c5988
hq <1422127065@qq.com>
2024-08-06 16:49:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import feapder
+from feapder import Request, Response
+
+
+class TestFeapder(feapder.AirSpider):
+
+    def start_spider(self):
+         url = "https://www.baidu.com/"
+         yield  feapder.Request(url=url, callback=self.parse)
+
+    def parse(self, request: Request, response: Response):
+        print(response.xpath('//title/text()').extract_first())
+        print(response.xpath('//meta[@name="description"]/@content').extract_first())
+
+
+if __name__ == '__main__':
+    print("ok")
